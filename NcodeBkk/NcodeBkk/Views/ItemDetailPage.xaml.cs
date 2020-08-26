65ec9bb1b5a7580d16fe56ea723c189928ccdac2
Spinning Cube Solutions <michael@spinningcube.co.nz>
2020-08-26 15:46:57 +1200

    Initial commit (Xamarin.Forms starting project)

---
+++ NcodeBkk/NcodeBkk/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using NcodeBkk.ViewModels;
+
+namespace NcodeBkk.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
